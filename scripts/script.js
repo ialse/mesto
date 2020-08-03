ca22098efd0886549c9cd8af8724d6f057568227
Alexey I <ialse@ya.ru>
2020-08-03 21:40:16 +0300

    Сделать popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
-let btnEdit = document.querySelector("profile__button-edit");
+let btnEdit = document.querySelector('.profile__button-edit');
+let btnSave = document.querySelector('.popup__btn-save');
+let btnClose = document.querySelector('.popup__btn-close');
 
-btnEdit.addEventListener("click", function() { document.querySelector("profile-edit").classList.add("profile-edit_opened") });
+btnEdit.addEventListener('click', function() {
+    document.querySelector('.popup').classList.add('popup_opened');
+    document.querySelector('.popup__inp-name').value = document.querySelector('.profile__title').textContent;
+    document.querySelector('.popup__inp-work').value = document.querySelector('.profile__subtitle').textContent;
+});
+
+btnSave.addEventListener('click', function() {
+    document.querySelector('.profile__title').textContent = document.querySelector('.popup__inp-name').value;
+    document.querySelector('.profile__subtitle').textContent = document.querySelector('.popup__inp-work').value;
+    document.querySelector('.popup').classList.remove('popup_opened');
+});
+
+btnClose.addEventListener('click', function() {
+    document.querySelector('.popup').classList.remove('popup_opened');
+});
