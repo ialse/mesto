3b710af8c25be796ba3d0be4abe75861b762a3c1
Alexey I <ialse@ya.ru>
2020-08-03 13:52:40 +0300

    сделать начало popup

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+let btnEdit = document.querySelector("profile__button-edit");
+
+btnEdit.addEventListener("click", function() { document.querySelector("profile-edit").classList.add("profile-edit_opened") });
