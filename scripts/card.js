46be31d479df02e8f61290c9eafe4f2bcdf5e350
ialse <ialse@ya.ru>
2020-09-08 17:57:52 +0300

    feat: ООП - продолжаю 1

--- scripts/card.js
+++ scripts/card.js
@@ -2,22 +2,23 @@ import initialCards from './initial-сards.js';
 
 class Card {
 
-    constructor (name, link) {
-        this._name = name;
-        this._link = link;
+    constructor (item, cardTemplate) {
+        this._name = item.name;
+        this._link = item.link;
+        this._cardTemplate = cardTemplate;
     }
 
 /*Создаем карточку*/
-_createCard(name, link) {
-    const card = cardTemplate.cloneNode(true);
+_createCard() {
+    const card = document.querySelector(this._cardTemplate).cloneNode(true);
     const elementImage = card.querySelector('.element__image');
     const elementTitle = card.querySelector('.element__title')
 
-    elementImage.src = link;
-    elementImage.alt = name;
-    elementTitle.textContent = name;
+    elementImage.src = this._link;
+    elementImage.alt = this._name;
+    elementTitle.textContent = this._name;
 
-    addHandlerCard(card); /*Навешиваем обработчики*/
+    _addHandlerCard(card); /*Навешиваем обработчики*/
 
     return card;
 }
@@ -38,64 +39,16 @@ _addHandlerCard(card) {
     });
 }
 
-/*Функция, добавляющая карточку*/
-function addCard(name, link) {
-    const card = createCard(name, link); /*Создаем карточку*/
+/*Функция, добавляющая карточку на страницу*/
+addCard() {
+    const card = _createCard(name, link); /*Создаем карточку*/
     elements.prepend(card); /*Добавляем сформированную карточку в начало страницы*/
 }
-
-/*Открываем попап*/
-function openPopup(popup) {
-    popup.classList.add('popup_opened');
 }
 
-/*Функция, закрывающая попап по Escape или при клике по оверлею*/
-function closeEscapeAndClickOverlay(evt) {
-    const popupActive = document.querySelector('.popup_opened');
-
-    if (evt.key === 'Escape' || evt.target === popupActive) {
-        closePopup(popupActive);
-    }
-}
-
-/*Навешиваем обработчики для закрытия попапа по Escape и по клику по оверлею*/
-function addHandlerPopup(popup) {
-    document.body.addEventListener('keyup', closeEscapeAndClickOverlay);
-    popup.addEventListener('click', closeEscapeAndClickOverlay);
-}
-
-/*Удаляем обработчики*/
-function removeHandlerPopup(popup) {
-    document.body.removeEventListener('keyup', closeEscapeAndClickOverlay);
-    popup.removeEventListener('click', closeEscapeAndClickOverlay);
-}
-
-/*Настраиваем попап Image*/
-function openImagePopup(e) {
-    const image = e.target.src;
-    const title = e.target.nextElementSibling.textContent;
-
-    elementImage.src = image
-    elementImage.alt = title;
-    elementTitle.textContent = title;
-
-    addHandlerPopup(popupImage);
-    openPopup(popupImage);
-}
-
-/*Функция, закрывающая попап */
-function closePopup(popup) {
-    popup.classList.remove('popup_opened');
-    removeHandlerPopup(popup);
-    clearErrorPopup(popup);
-}
-
-
-/*Функция, которая отрисовывает по шаблону первоначальные карточки */
+/*Добавляем на страницу начальные карточки */
 initialCards.forEach(function(item) {
-    const card = new Card (item.name, item.link);
+    const card = new Card (item, '#card-template');
+
+    card.addCard();
 });
-
-}
-
-btnCloseImage.addEventListener('click', () => closePopup(popupImage));
