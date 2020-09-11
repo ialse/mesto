562bf2345fd89cef682a243921a96dbf90a60f37
ialse <ialse@ya.ru>
2020-09-11 15:42:37 +0300

    fix: исправил Card - обработчики сделал отдельно, заменил event на this._card

--- scripts/card.js
+++ scripts/card.js
@@ -5,12 +5,32 @@ export default class Card {
         this._link = data.link;
         this._cardTemplate = cardTemplate;
         this._createImagePopup = createImagePopup;
+        this._card = document.querySelector(this._cardTemplate).content.cloneNode(true);
+    }
+
+    /*Обработчик события, отвечающий за работу лайка*/
+    _addHandlerLike () {        
+        const like = this._card.querySelector('.element__button-like');
+        like.addEventListener('click', () => {
+            like.classList.toggle('element__button-like_active');
+        });
+    }
+
+    /*Обработчик события, удаляющий карточку*/
+    _addHandlerBtnRemove () { 
+        const btnRemove = this._card.querySelector('.element__button-remove');
+        btnRemove.addEventListener('click', () => {
+            btnRemove.closest('.element').remove();
+        });
+    }
+
+    /*Обработчик события: клик по картинке - открывается попап с картинкой*/
+    _addHandlerClickImage () {        
+        this._card.querySelector('.element__image').addEventListener('click', this._createImagePopup);
     }
 
     /*Создаем карточку*/
     createCard () {
-        this._card = document.querySelector(this._cardTemplate).content.cloneNode(true);
-
         const elementImage = this._card.querySelector('.element__image');
         const elementTitle = this._card.querySelector('.element__title');
 
@@ -18,24 +38,11 @@ export default class Card {
         elementImage.alt = this._name;
         elementTitle.textContent = this._name;
 
-        this._addHandlerCard(this._card); /*Навешиваем обработчики*/
+        /*Навешиваем обработчики*/
+        this._addHandlerLike(); 
+        this._addHandlerClickImage();
+        this._addHandlerBtnRemove();
 
         return this._card;
-    }
-
-    /*Функция, навешивающая обработчики на карточки*/
-    _addHandlerCard (card) {
-        /*каждой кнопке навешиваем обработчик события, отвечающий за работу лайка*/
-        card.querySelector('.element__button-like').addEventListener('click', event => {
-            event.currentTarget.classList.toggle('element__button-like_active');
-        });
-
-        /*каждой картинке навешиваем обработчик события: клик по картинке - открывается попап с картинкой*/
-        card.querySelector('.element__image').addEventListener('click', this._createImagePopup);
-
-        /*каждой кнопке удаления навешиваем обработчик события, удаляющий карточку*/
-        card.querySelector('.element__button-remove').addEventListener('click', event => {
-            event.currentTarget.closest('.element').remove();
-        });
-    }
+    }    
 }
